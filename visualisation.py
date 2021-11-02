05cf8423decda3a20072b28fbb24ce8de71f1812
ahernot <70817470+ahernot@users.noreply.github.com>
2021-11-02 16:39:00 +0100

    Update visualisation.py

--- visualisation.py
+++ visualisation.py
@@ -1,24 +1,10 @@
-# import audiofile
-# import wave
+import os
+import subprocess
 
-# from preferences import *
-
-# import os
-# from subprocess import check_call
-# from tempfile import mktemp
-# # from scikits.audiolab import wavread, play
-# from scipy.signal import remez, lfilter
-# from pylab import *
-
-# # convert mp3, read wav
-# mp3filename = RESOURCES_PATH + 'DonneeAccoustique/CRSon/2020_01_25_2 Moteur.mp3'
-# wname = mktemp('.wav')
-# check_call(['avconv', '-i', mp3filename, wname])
-# # sig, fs, enc = wavread(wname)
+from preferences import *
 
 
-# with wave.open(wname) as audio_file:
-#     x = audio_file.readframes(audio_file.getnframes())
-#     print(x)
-    
-# os.unlink(wname)
+
+filepath = RESOURCES_PATH + 'raw-data/2020_14_02_3A/2020_14_02_1.wav'  # use OS path objects
+
+
