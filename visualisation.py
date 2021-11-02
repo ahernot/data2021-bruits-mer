bf39b1566257289057e6d1b81557e9c0a153ae02
ahernot <70817470+ahernot@users.noreply.github.com>
2021-11-02 16:56:09 +0100

    begin reading WAV files

--- visualisation.py
+++ visualisation.py
@@ -1,10 +1,35 @@
 import os
 import subprocess
 
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+
 from preferences import *
 
 
+# Read the wav file
+filepath = RESOURCES_PATH + 'samples/2020_01_28_3A/2020_01_28_1.wav'  # use OS path objects
+samplerate, data = wavfile.read(filepath)
 
-filepath = RESOURCES_PATH + 'raw-data/2020_14_02_3A/2020_14_02_1.wav'  # use OS path objects
 
+# Plot part of the signal
+RUN_1 = False
+if RUN_1 == True:
+    sample = data[:10000, :]
+    xcoords = np.arange(sample.shape[0])
+
+    plt.figure(figsize=(15, 10))
+    plt.plot(xcoords, sample[:, :])
+    plt.show()
+
+
+# Plot fourier transform of the signal (to continue)
+RUN_2 = True
+if RUN_2 == True:
+    sp = np.fft.fft (data[:, 0])  # spectrum
+    freq = np.fft.fftfreq( data.shape[0] )  # frequencies
+    plt.figure(figsize=(15, 10))
+    plt.plot(freq, sp.real, freq, sp.imag)
+    plt.show()
 
