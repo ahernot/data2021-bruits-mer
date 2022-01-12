fd1a627bfe3f92e165f6697e30394c20f62b6d46
ahernot <70817470+ahernot@users.noreply.github.com>
2022-01-12 12:22:35 +0100

    Create db_test.py

---
+++ physics/db_test.py
@@ -0,0 +1,12 @@
+# test from https://datalab.marine.rutgers.edu/2020/10/introduction-to-python-part-2/
+
+
+# Notebook setup
+import xarray as xr
+# !pip install netcdf4
+
+# # Open dataset
+# ds = xr.open_dataset('https://dods.ndbc.noaa.gov/thredds/dodsC/data/stdmet/44025/44025.ncml')
+
+# # Subset the dataset to 1 year
+# ds = ds.sel(time=slice('2019-01-01','2020-01-01'))
