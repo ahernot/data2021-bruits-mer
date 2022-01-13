1b04efec7dc871bf73cf69579d06f43190587ad8
ahernot <70817470+ahernot@users.noreply.github.com>
2022-01-13 19:10:02 +0100

    modify units

--- absorption.py
+++ absorption.py
@@ -2,11 +2,11 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-def absorption (f: float, depth: float, temperature: float, salinity: float, pH: float):
+def calc_absorption (f: float, z: float, T: float, S: float, pH: float):
     """
     Ainslie and McColm 1998
-    :param f: Signal frequency (in kHz)
-    :param depth: Depth (in km)
+    :param f: Signal frequency (in Hz)
+    :param z: Altitude (in m)
     :param temperature: Temperature (in ℃)
     :param salinity: Salinity (in ppt)
     :param pH: Acidity (pH)
@@ -20,14 +20,16 @@ def absorption (f: float, depth: float, temperature: float, salinity: float, pH:
     0 < distance < 7km
     """
 
+    f /= 1000
+
     # Boric acid (relaxation absorption)
-    f1 = 0.78 * np.sqrt(salinity / 35) * np.exp(temperature / 26)  # Boric acid relaxation frequency (kHz)
+    f1 = 0.78 * np.sqrt(S / 35) * np.exp(T / 26)  # Boric acid relaxation frequency (kHz)
     A1 = 0.106
     alpha_1 = A1 * (f1 * np.power(f, 2)) / (np.power(f1, 2) + np.power(f, 2))
 
     # Magnesium sulfate (relaxation absorption)
-    f2 = 42 * np.exp(temperature / 17)  # Magnesium sulfate relaxation frequency (kHz)
-    A2 = 0.52 * (1 + temperature / 43) * (salinity / 35)
+    f2 = 42 * np.exp(T / 17)  # Magnesium sulfate relaxation frequency (kHz)
+    A2 = 0.52 * (1 + T / 43) * (S / 35)
     alpha_2 =  A2 * (f2 * np.power(f, 2)) / (np.power(f2, 2) + np.power(f, 2))
 
     # Pure water (visquous absorption)
@@ -36,38 +38,8 @@ def absorption (f: float, depth: float, temperature: float, salinity: float, pH:
 
     # Total seawater absorption (in dB/km)
     alpha = alpha_1 * np.exp((pH - 8) / 0.56) +\
-            alpha_2 * np.exp(-1 * depth / 6)  +\
-            alpha_3 * np.exp(-1 * (temperature/27 + depth/17))
+            alpha_2 * np.exp(z / 6000)  +\
+            alpha_3 * np.exp(z / 17000 - T / 27)
 
 
     return alpha
-
-
-
-# kwargs = {
-#     'depth': 0.05,
-#     'temperature': 8,
-#     'salinity': 35,
-#     'pH': 8
-# }
-
-# print(absorption(1, **kwargs))
-
-# frequencies = np.linspace (1e-1, 1e3, 100)
-# absorptions = absorption (frequencies, **kwargs)
-
-# plt.figure (figsize=(15, 10))
-# plt.loglog(frequencies, absorptions)
-# plt.xlabel('Frequency (kHz)')
-# plt.ylabel('Absorption (dB/km)')
-# plt.show()
-
-
-
-# absorption_10hz = absorption(10, **kwargs)
-# print(absorption_10hz)
-
-# plt.figure(figsize=(15, 10))
-# distances = np.linspace(0, 7, 200)  # in km
-# plt.plot(distances, absorption_10hz * distances)
-# plt.show()
